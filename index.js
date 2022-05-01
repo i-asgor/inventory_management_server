eef2d8040ccecec88eefd76e701eed8319a81292
Asgor Ali <i.joni40@gmail.com>
2022-05-02 00:24:03 +0600

    single data show by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,16 @@ async function run(){
             const items = await cursor.toArray();
             res.send(items)
         })
+
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: ObjectId(id) };
+            const singleItem = await itemCollection.findOne(query);
+            console.log(query, singleItem)
+            res.send(singleItem);
+        });
+
 
     }finally{
         // await client.close();
