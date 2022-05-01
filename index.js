ed2e9c6f52339f5d27dc2d125a86b80a5d2d772d
Asgor Ali <i.joni40@gmail.com>
2022-05-01 21:06:45 +0600

    mongodb connected using .env file secret user & password

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dsgsp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Mongodb Connected")
+  client.close();
+});
+
 
 app.get('/', (req,res) => {
     res.send("Hello World");
