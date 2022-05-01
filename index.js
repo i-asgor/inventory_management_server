163afb9816931cf342ef9e18de0287ea28e51f33
Asgor Ali <i.joni40@gmail.com>
2022-05-01 23:18:36 +0600

    all data show

--- index.js
+++ index.js
@@ -12,12 +12,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dsgsp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("Mongodb Connected")
-  client.close();
-});
+
+async function run(){
+    try{
+        await client.connect();
+        const itemCollection = client.db("ElectronicsInventory").collection("item");
+        // get items
+        app.get('/inventory', async(req,res)=>{
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items)
+        })
+
+    }finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
 
 
 app.get('/', (req,res) => {
